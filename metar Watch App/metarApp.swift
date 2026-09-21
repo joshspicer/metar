05fbeafd61b233d5a511e967725519ccc5bc9711
Josh Spicer <hello@joshspicer.com>
2026-09-20 17:58:46 -0700

    Initial Commit

---
+++ metar Watch App/metarApp.swift
@@ -0,0 +1,17 @@
+//
+//  metarApp.swift
+//  metar Watch App
+//
+//  Created by Josh Spicer on 9/20/26.
+//
+
+import SwiftUI
+
+@main
+struct metar_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
